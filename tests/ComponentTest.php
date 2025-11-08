fda33bf388a5ef5ce2cdf93f81edecbfefc923c7
Tharindu Wijayarathna <wikum.dev@gmail.com>
2025-11-08 22:57:35 +0530

    refactor: clean up whitespace in ComponentTest.php

--- tests/ComponentTest.php
+++ tests/ComponentTest.php
@@ -9,28 +9,28 @@ use EvolveUI\EvolveUI\View\Components\Layout\App;
 it('can render button component', function () {
     $component = new Button();
     $view = $component->render();
-    
+
     expect($view)->toBeInstanceOf(\Illuminate\Contracts\View\View::class);
 });
 
 it('can render auth layout component', function () {
     $component = new Auth();
     $view = $component->render();
-    
+
     expect($view)->toBeInstanceOf(\Illuminate\Contracts\View\View::class);
 });
 
 it('can render app layout component', function () {
     $component = new App();
     $view = $component->render();
-    
+
     expect($view)->toBeInstanceOf(\Illuminate\Contracts\View\View::class);
 });
 
 it('button component has correct default classes', function () {
     $component = new Button();
     $classes = $component->buttonClasses();
-    
+
     expect($classes)->toContain('inline-flex');
     expect($classes)->toContain('items-center');
     expect($classes)->toContain('justify-center');
