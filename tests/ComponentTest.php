854897a8f13c15cb0811994f67734a3626411681
Tharindu Wijayarathna <wikum.dev@gmail.com>
2025-11-08 22:58:11 +0530

    refactor: streamline component instantiation in ComponentTest.php

--- tests/ComponentTest.php
+++ tests/ComponentTest.php
@@ -2,37 +2,36 @@
 
 namespace EvolveUI\EvolveUI\Tests;
 
-use EvolveUI\EvolveUI\View\Components\Ui\Button;
-use EvolveUI\EvolveUI\View\Components\Layout\Auth;
 use EvolveUI\EvolveUI\View\Components\Layout\App;
+use EvolveUI\EvolveUI\View\Components\Layout\Auth;
+use EvolveUI\EvolveUI\View\Components\Ui\Button;
 
 it('can render button component', function () {
-    $component = new Button();
+    $component = new Button;
     $view = $component->render();
 
     expect($view)->toBeInstanceOf(\Illuminate\Contracts\View\View::class);
 });
 
 it('can render auth layout component', function () {
-    $component = new Auth();
+    $component = new Auth;
     $view = $component->render();
 
     expect($view)->toBeInstanceOf(\Illuminate\Contracts\View\View::class);
 });
 
 it('can render app layout component', function () {
-    $component = new App();
+    $component = new App;
     $view = $component->render();
 
     expect($view)->toBeInstanceOf(\Illuminate\Contracts\View\View::class);
 });
 
 it('button component has correct default classes', function () {
-    $component = new Button();
+    $component = new Button;
     $classes = $component->buttonClasses();
 
     expect($classes)->toContain('inline-flex');
     expect($classes)->toContain('items-center');
     expect($classes)->toContain('justify-center');
 });
-
